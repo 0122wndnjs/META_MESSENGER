18ff3a61849b78eb4d6c24bbf4ebaa5e38521ca0
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-12-09 10:36:08 +0900

    adding MessageList component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import MessageList from './MessageList'
 
 function HomePage() {
   return (
-    <div>
-        <h1>Welcome to the MESSENGER</h1>
-    </div>
+    <main>
+      {/* Message */}
+      <MessageList />
+      {/* ChatInput */}
+    </main>
   )
 }
 
